c9f7cd441b657774356eef4974c511f9ac20a12a
dudu0875 <114429581+dudu0875@users.noreply.github.com>
2022-10-04 15:49:16 -0300

    Add files via upload

---
+++ PROJECT-TEMPLATE_C11-main (2)/PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation(//liste os nomes dos arquivos das imagens que voce deseja add)
+sriteName.addAmage(seaImg)
+if (sea.x <0){
+
+sea .x = sea.width/2;
+
+}
+
+
